e7363eed86592c71a880b521df52c00739be16be
Jeremiah Crowell <jeremiah.c2001@gmail.com>
2025-08-23 12:15:54 -0500

    plaid

--- app.py
+++ app.py
@@ -3,41 +3,40 @@ import plotly.express as px
 import pandas as pd
 import pathlib as pl
 import dash_bootstrap_components as dbc
+from datafetchers import fetch_transaction_df_all
 
 # Load data
-data_dir = pl.Path("statements")
-amex_dir = data_dir / "amex"
-amex_files = list(amex_dir.glob("*.csv"))
+# data_dir = pl.Path("statements")
+# amex_dir = data_dir / "amex"
+# amex_files = list(amex_dir.glob("*.csv"))
 
-amex_df = pd.concat(
-    [pd.read_csv(file, parse_dates=["Date"]) for file in amex_files],
-)
-amex_df["Month"] = amex_df["Date"].dt.to_period("M")
-amex_df["Month_Name"] = amex_df["Date"].dt.strftime("%B %Y")
-amex_df["Category_1"] = amex_df["Category"].str.split("-").str[0]
-amex_df["Category_2"] = amex_df["Category"].str.split("-").str[1]
-amex_df["Simple_Desc"] = amex_df["Description"].str.split(" ").str[:2].str.join(" ")
-amex_df = amex_df[amex_df["Amount"] > 0]
+# df = pd.concat(
+#     [pd.read_csv(file, parse_dates=["Date"]) for file in amex_files],
+# )
+df = fetch_transaction_df_all()
+purchases_df = df[df.amount > 0]
 
 # Setup Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-month_periods = amex_df["Month"].sort_values().unique()
-month_names = [
-    amex_df[amex_df["Month"] == m]["Month_Name"].iloc[0] for m in month_periods
-]
+month_periods = df["Month"].sort_values().unique()
+month_names = [df[df["Month"] == m]["Month_Name"].iloc[0] for m in month_periods]
 max_month = month_names[-1]
 
 category_to_color = {
-    "Merchandise & Supplies": "lightblue",
-    "Business Services": "lightgreen",
-    "Transportation": "lightcoral",
-    "Communications": "plum",
-    "Restaurant": "orange",
-    "Fees & Adjustments": "tan",
-    "Travel": "pink",
-    "Entertainment": "gold",
-    "Other": "gray",
+    "GENERAL_SERVICES": "lightgreen",
+    "RENT_AND_UTILITIES": "plum",
+    "LOAN_PAYMENTS": "lightblue",
+    "GENERAL_MERCHANDISE": "lightblue",
+    "FOOD_AND_DRINK": "orange",
+    "TRANSPORTATION": "lightcoral",
+    "ENTERTAINMENT": "gold",
+    "TRANSFER_OUT": "pink",
+    "PERSONAL_CARE": "lightpink",
+    "MEDICAL": "hotpink",
+    "BANK_FEES": "lightgray",
+    "GOVERNMENT_AND_NON_PROFIT": "lightcyan",
+    "HOME_IMPROVEMENT": "lightsalmon",
 }
 
 app.layout = dbc.Container(
@@ -52,6 +51,12 @@ app.layout = dbc.Container(
                     className="mb-4",
                 ),
                 width={"size": 6, "offset": 3},
+            )
+        ),
+        dbc.Row(
+            dbc.Col(
+                html.H2("American Express Treemap", className="text-center mb-4"),
+                width=12,
             )
         ),
         dbc.Row(dbc.Col(dcc.Graph(id="treemap-content"), width=12)),
@@ -101,14 +106,14 @@ app.layout = dbc.Container(
 
 @callback(Output("treemap-content", "figure"), Input("month-selection", "value"))
 def update_treemap(value):
-    dff = amex_df[amex_df.Month_Name == value]
+    dff = purchases_df[purchases_df.Month_Name == value]
     chart = px.treemap(
         dff,
-        path=["Category_2", "Simple_Desc"],
-        values="Amount",
-        color="Category_1",
+        path=["personal_finance_category.primary", "merchant_name"],
+        values="amount",
+        color="personal_finance_category.primary",
         color_discrete_map=category_to_color,
-        hover_data=["Amount", "Description", "Date"],
+        hover_data=["name", "amount", "date"],
     )
     chart.update_traces(marker=dict(cornerradius=5), textfont_size=20)
     chart.update_layout(
---
+++ datafetchers.py
@@ -0,0 +1,63 @@
+#!python3
+
+import sqlite3
+import json
+import datetime
+import base64
+import pandas as pd
+
+
+dbfile = "../finances-data/plaid-sync.db"
+
+interested_cols = [
+    "amount",
+    "date",
+    "merchant_name",
+    "name",
+    "location.city",
+    "location.region",
+    "location.country",
+    "personal_finance_category.primary",
+    "personal_finance_category.detailed",
+]
+
+
+def fetch_transaction_df_by_daterange(
+    start_date: datetime.date, end_date: datetime.date
+) -> pd.DataFrame:
+    conn = sqlite3.connect(dbfile)
+    c = conn.cursor()
+    r = c.execute(
+        """
+        select plaid_json from transactions
+        where json_extract(plaid_json, '$.date') between ? and ?
+    """,
+        (start_date, end_date),
+    )
+    jsons = [json.loads(d[0]) for d in r.fetchall()]
+    return add_columns(pd.json_normalize(jsons))
+
+
+def fetch_transaction_df_all() -> pd.DataFrame:
+    conn = sqlite3.connect(dbfile)
+    c = conn.cursor()
+    r = c.execute("select plaid_json from transactions")
+    jsons = [json.loads(d[0]) for d in r.fetchall()]
+    df = pd.json_normalize(jsons)
+    # convert all non amount columns to str
+    for col in df.columns:
+        if col != "amount":
+            df[col] = df[col].astype(str)
+    return add_columns(df)
+
+
+def add_columns(df) -> pd.DataFrame:
+    df["date"] = pd.to_datetime(df["date"])
+    df["Month"] = df["date"].dt.to_period("M")
+    df["Month_Name"] = df["date"].dt.strftime("%B %Y")
+    df = df[df.merchant_name != base64.b64decode("UGF0cmVvbg==").decode("utf-8")]
+    return df
+
+
+if __name__ == "__main__":
+    print(fetch_transaction_df_all().columns)
